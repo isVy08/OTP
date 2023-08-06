83f18da346844b13da6bf93af42a5fe1473714f5
Vy Vo <52537680+isVy08@users.noreply.github.com>
2023-08-07 07:54:46 +1000

    Initial commit

--- amortized_topic.py
+++ amortized_topic.py
@@ -1,7 +1,7 @@
 from octis.dataset.dataset import Dataset
 from octis.evaluation_metrics.coherence_metrics import Coherence
 from octis.evaluation_metrics.diversity_metrics import TopicDiversity
-import time
+import torch
 import gensim.corpora as corpora
 from utils_io import write_pickle, load_pickle
 from utils_model import load_topic_config
@@ -22,6 +22,7 @@ if dataset_name != 'lda':
     dataset.fetch_dataset(dataset_name)
     K = 10
     corpus = dataset.get_corpus()
+    # sum([len(text) for text in corpus]) / len(corpus)
     vocab = dataset.get_vocabulary()
     id2word = corpora.Dictionary(dataset.get_corpus())
     word2id = {v: k for k, v in id2word.items()}
@@ -49,9 +50,12 @@ if dataset_name != 'lda':
 
 
 elif dataset_name == 'lda': 
+    import os
+    from utils_io import load_pickle, write_pickle
     
     config_index = int(sys.argv[2])
     arch = sys.argv[3]
+    ver = sys.argv[4]
     config = load_topic_config(config_index)
 
     K = config[0]    
@@ -59,6 +63,7 @@ elif dataset_name == 'lda':
     L = n_tokens = config[2]
     
     data_path = f'data/lda{config_index}.pickle'
+    model_path = f'model/{arch}{config_index}{ver}.pt'
     true_beta, true_topics, word_matrix  = load_pickle(data_path)
     K, V = true_topics.shape
     word2id = {i: i for i in range(V)}
@@ -68,21 +73,31 @@ elif dataset_name == 'lda':
     vocab = [str(i) for i in range(V)]
 
     dataset = Dataset(corpus = corpus, vocabulary = vocab)
+    if not os.path.isfile(model_path):
+        if arch == 'neural':
+            model = NeuralLDA(num_topics=K, num_epochs=300, use_partitions=False, 
+                              batch_size=50, lr=1e-3, learn_priors=False, prior_mean=1/K)
+        else:
+            model = ProdLDA(num_topics=K, num_epochs=300, use_partitions=False, 
+                            batch_size=50, lr=1e-3, learn_priors=False, prior_mean=1/K)
+        print('Training begins ...')
 
-    if arch == 'neural':
-        model = NeuralLDA(num_topics=K, num_epochs=300, use_partitions=False, batch_size=50, lr=1e-3)
+        model_output = model.train_model(dataset)
+        torch.save(model, model_path)
+    else: 
+        model = torch.load(model_path)
+    alpha = model.model.model.prior_mean.data
+    print(alpha.mean())
+    if arch == 'prod':
+        topic_matrix = torch.nn.functional.softmax(model.model.model.beta, dim=1)
     else:
-        model = ProdLDA(num_topics=K, num_epochs=300, use_partitions=False, batch_size=50, lr=1e-3)
-    print('Training begins ...')
+        topic_matrix = torch.nn.functional.softmax(model.model.model.beta_batchnorm(model.model.model.beta), dim=1)
+    topic_matrix = topic_matrix.detach().cpu().numpy()
 
-    model_output = model.train_model(dataset)
-    topic_matrix = model_output['topic-word-matrix']
-
-    import torch, scipy
+    import scipy
+    import torch
     from utils_model import compute_topic_estimates
     
-    if arch == 'prod':
-        topic_matrix = scipy.special.softmax(topic_matrix, axis = -1)     
     P = torch.Tensor(topic_matrix)
     Q = torch.Tensor(true_topics)    
     compute_topic_estimates(P, Q)
@@ -92,7 +107,7 @@ elif dataset_name == 'lda':
     import numpy as np
     truth = {k : sorted(np.where(true_topics[k, :])[0].tolist()) for k in range(K)}
     estimated = match_topics(topic_matrix, truth)
-    k = min(K, 15)
+    k = min(K, 20)
     topics = list(range(k))
     visualize_topics(topics, V, truth, estimated, f'{arch}{config_index}')
     
