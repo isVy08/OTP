9cb5da87a3f7ace3bb396f9f83d23b271c33e917
Vy Vo <52537680+isVy08@users.noreply.github.com>
2023-09-27 11:36:32 +1000

    Initial commit

--- train_topic_model.py
+++ train_topic_model.py
@@ -7,46 +7,48 @@ from tqdm import tqdm
 import gensim.corpora as corpora
 from octis.dataset.dataset import Dataset
 from torch.utils.data import DataLoader
-from topic_model import TopicModel
 from utils_io import load_model
-
 
 from octis.evaluation_metrics.diversity_metrics import TopicDiversity
 from octis.evaluation_metrics.coherence_metrics import Coherence
-    
+from geomloss import SamplesLoss
+
 
 
 def train_epoch(model, optimizer, scheduler,
-                X, train_loader, weight, device, embedding_ts = None):
+                X, train_loader, weight, device, ground_cost):
     
     model.train()
     bce = nn.BCELoss()
     l2 = nn.MSELoss()
     kl = nn.KLDivLoss(reduction="batchmean")
+    sk = SamplesLoss("sinkhorn", p=2, blur=0.01, scaling=.9, backend="tensorized")
 
     Loss = 0
     
-    for idx in tqdm(train_loader):
+    for idx in train_loader:
       x = X[idx, :].to(device)
       xhat, theta, logits = model(x)   
+      loss = bce(xhat, x) + l2(xhat, x)
 
-      if embedding_ts is not None:
-        e = torch.repeat_interleave(embedding_ts, x.size(0), dim = 0)
-        y = torch.matmul(x, e)
-        yhat = torch.matmul(xhat, e)
-        loss = l2(yhat, y)
-      else: 
-
-        loss = bce(xhat, x)
-      
-      B = theta.size(0)
+      B = x.size(0)
       a = torch.ones((B,), device = device) / B 
-      M = torch.zeros((B, B), device = device)
-      for i in range(B):
-        for j in range(B):
-          M[i, j] = kl(logits[i, :], theta[j])
-                 
-      ws  = ot.emd2(a, a, M)
+
+      if ground_cost == 'kl':
+        M = torch.zeros((B, B), device = device)
+        for i in range(B):
+          for j in range(B):
+            M[i, j] = kl(logits[i, :], theta[j, :])
+        ws  = ot.emd2(a, a, M)
+      elif ground_cost == 'sk':
+        probs = torch.exp(logits)    
+        ws = sk(probs, theta)
+        
+      else:
+        probs = torch.exp(logits)    
+        M = ot.dist(probs, theta)
+        ws  = ot.emd2(a, a, M)
+
       loss += weight * ws
 
       model.module.backward_fn.lstm.flatten_parameters()
@@ -83,10 +85,11 @@ def evaluate(model, corpus, K, print_output = False):
 
 if __name__ == "__main__":
     
-    device_ids = [0,1,2,3]
+    device_ids = [2,3,1]
     import sys 
     action = sys.argv[1]
     dataset_name = sys.argv[2] # '20NewsGroup', 'BBC_News', 'DBLP'
+    
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu', device_ids[0])
 
     dataset = Dataset()
@@ -100,7 +103,7 @@ if __name__ == "__main__":
 
     from utils_model import vectorizer
     X = vectorizer(corpus, word2id, "one-hot")
-
+    
     print(f'Training {X.shape[0]} documents ...')
 
 
@@ -110,16 +113,18 @@ if __name__ == "__main__":
     B = 50
     D = 50
     tau = 2.0
-    model_path = f'model/topic_{dataset_name}.pt'
+    ver = sys.argv[3]
+    model_path = f'model/topic_{dataset_name}_{ver}.pt'
     
     train_indices = list(range(X.size(0)))
     train_loader = DataLoader(train_indices, batch_size=B, shuffle=True)
 
+    from topic_model import TopicModel
     model = TopicModel(V, K, H, D, tau, True)
     model.to(device)
     
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    scheduler = None
+    scheduler = None # torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.98)
     num_epochs = 1000
     weight = 0.1
 
@@ -131,16 +136,19 @@ if __name__ == "__main__":
     if action == 'train':
       model = nn.DataParallel(model, device_ids)
       start = time.time()
+        
+      ground_cost = 'kl' if dataset != 'DBLP' else 'l2' # to speed up training on large data like DBLP
       
       for epoch in range(num_epochs):
-          loss = train_epoch(model, optimizer, scheduler, X, train_loader, weight, device)
+          loss = train_epoch(model, optimizer, scheduler, X, train_loader, 
+                             weight, device, ground_cost)
           print(f"Epoch: {epoch} - Loss: {loss:.5f}")
           if loss < prev_loss:
             prev_loss = loss
             print('Saving model ...')
             torch.save({'model_state_dict': model.module.state_dict() ,
                         'optimizer_state_dict': optimizer.state_dict(),
-                        'scheduler_state_dict': scheduler.state_dict() if scheduler is not None else None,
+                        # 'scheduler_state_dict': scheduler.state_dict(),
                         'prev_loss': prev_loss,
                         }, model_path)
 
@@ -152,12 +160,3 @@ if __name__ == "__main__":
       model.eval()
       evaluate(model, corpus, K, True)
                 
-        
-        
-
-    
-      
-
-          
-
-
